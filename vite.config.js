77f6a751d2fe6b13dfda1b992e46393e5a42f2aa
othelarian <le.maitre.killian@gmail.com>
2026-05-20 17:15:38 +0200

    fixing asset path and checkout version

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { coffee } from "vite-plugin-coffee3";
 console.log(coffee);
 
 export default defineConfig({
+  base: '',
   //entry: "src/app.coffee",
   //dest: "app.js",
   modulePreload: false,
